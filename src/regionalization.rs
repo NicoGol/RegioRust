68408dec13367ab25805433be738d09b60bfe319
ngolenvaux <nicolas.golenvaux@uclouvain.be>
2023-05-05 14:49:24 +0200

    add the cheap upper bound

--- src/regionalization.rs
+++ src/regionalization.rs
@@ -230,12 +230,12 @@ impl Relaxation for RegionalizationRelax<'_> {
     }
 
     // if this is too slow, then just ditch it.
-    // fn fast_upper_bound(&self, state: &Self::State) -> isize {
-    //     let n = self.pb.k - state.n_regions();
-    //     let mut h = state.h().clone();
-    //     h.sort_unstable_by(|a, b| a.total_cmp(b).reverse());
-    //     let result = h.iter().take(n).sum::<f64>();
-    //     //let result = state.h().iter().sum::<f64>();
-    //     (result * P).round() as isize
-    // }
+    fn fast_upper_bound(&self, state: &Self::State) -> isize {
+        let n = self.pb.k - state.n_regions();
+        let mut h = state.h().clone();
+        h.sort_unstable_by(|a, b| a.total_cmp(b).reverse());
+        let result = h.iter().take(n).sum::<f64>();
+        //let result = state.h().iter().sum::<f64>();
+        (result * P).round() as isize
+    }
 }
